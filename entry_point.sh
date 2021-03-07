172424da1fac39061c845b9e9512223f684e41db
Petter Kvalvaag <pmkv@equinor.com>
2021-03-07 12:12:08 +0100

    Use envoy instead of nginx
    TODO use ssl

--- entry_point.sh
+++ entry_point.sh
@@ -2,4 +2,4 @@
 python manage.py collectstatic &
 uwsgi --ini /code/deploy/wateringcontrol_uwsgi.ini &
 python manage.py runserver 0.0.0.0:8001 &
-nginx -g 'daemon off;'
+envoy -c /code/deploy/envoy_conf.yaml
